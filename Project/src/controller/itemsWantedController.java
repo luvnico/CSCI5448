7bf0c37d53a426d09af6f157a1889f07f52eb26e
luvnico <ys2k6@mail.missouri.edu>
2018-05-30 22:35:43 -0600

    Revised items wanted controller to response differently for each action

--- Project/src/controller/itemsWantedController.java
+++ Project/src/controller/itemsWantedController.java
@@ -17,21 +17,20 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
-import database.Account;
+import database.Posting;
 
 /**
  * Servlet implementation class itemsWantedController
  */
-@WebServlet("/itemsWantedController")
-public class itemsWantedController extends HttpServlet {
+@WebServlet("/itemsWanted")
+public class ItemsWantedController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	private DataSource ds;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public itemsWantedController() {
+    public ItemsWantedController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -39,11 +38,8 @@ public class itemsWantedController extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
 		try {
 			InitialContext initContext = new InitialContext();
-
 			Context env = (Context) initContext.lookup("java:comp/env");
-
 			ds = (DataSource) env.lookup("jdbc/CraigsList");
-
 		} catch (NamingException e) {
 			throw new ServletException();
 		}
@@ -52,9 +48,7 @@ public class itemsWantedController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(true);
-		PrintWriter out = response.getWriter();
-		request.getRequestDispatcher("/itemWanted.jsp").forward(request, response);
+		System.out.println("get pid:"+request.getParameter("pid"));
 	}
 
 	/**
@@ -62,8 +56,7 @@ public class itemsWantedController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(true);
-		PrintWriter out = response.getWriter();
-		
+		PrintWriter out = response.getWriter();	
 		Connection conn = null;
 
 		try {
@@ -73,54 +66,44 @@ public class itemsWantedController extends HttpServlet {
 			out.println("Unable to connect to database.");
 			System.out.println("Unable to connect to database.");
 			return;
-			//throw new ServletException();
 		}
-		
-		
-		/*if((session.getAttribute("loginEmail") == null) && (session.getAttribute("createEmail") == null)){
-			request.getRequestDispatcher("/signup.jsp").forward(request, response);
+		String action = request.getParameter("action");
+		/*if(action.equals("create")){
+			//you need to add hidden input in the form @postpreview.jsp first
+		} else if(action.equals("edit")){
+			
+		} else{
+			System.out.println("Unrecognised action");
 		}*/
-			
-		//else{	
+		Posting p_conn = new Posting(conn);
+		String contactEmail = request.getParameter("contactEmail");
+		String contactName = request.getParameter("contactName");
+		String title = request.getParameter("title");
+		String type = "items wanted";
+		String description = request.getParameter("description");
+		String status = "draft";
+		
+		request.setAttribute("title",title);
+		request.setAttribute("description",description);
+		request.setAttribute("status", status);
 
-			Account account = new Account(conn);
-			String contactEmail = request.getParameter("contactEmail");
-			String contactName = request.getParameter("contactName");
-			String title = request.getParameter("title");
-			String type = "itemWanted";
-			String description = request.getParameter("description");
-			String status = "draft";
-			
-			request.setAttribute("title",title);
-			request.setAttribute("description",description);
-			
-			
-			
-			
-			//Posting posting = new Posting(email,title, type, description);
-		
-			
-			try{
-				String email = "";
-				if( session.getAttribute("loginEmail") != null ){
-					email = (String)session.getAttribute("loginEmail");
-					
-				}
-				
-				else if( session.getAttribute("signupEmail") != null ){
-					email = (String)session.getAttribute("signupEmail");
-					
-				}
-				account.post(email, title, type, description,contactEmail, contactName, status);
-				request.getRequestDispatcher("/postpreview.jsp").forward(request, response);
-			}catch (SQLException e) {
-			
-				request.getRequestDispatcher("/error.jsp").forward(request, response);
+		try{
+			String email = "";
+			if( session.getAttribute("loginEmail") != null ){
+				email = (String)session.getAttribute("loginEmail");
 			}
-		//}
-		
-	
-	
+			else if( session.getAttribute("signupEmail") != null ){
+				email = (String)session.getAttribute("signupEmail");
+			}
+			int pid = p_conn.newpost(email, title, type, description,contactEmail, contactName, status);
+			// get pid of the new posting created & forward to preview
+			request.setAttribute("pid", pid);
+			request.getRequestDispatcher("/postpreview.jsp").forward(request, response);
+		}catch (SQLException e) {
+			System.out.println("ERROR from itemsWantedController: "+e.getMessage());
+			request.getRequestDispatcher("/error.jsp").forward(request, response);
+		}
+
 	}
 
 }
