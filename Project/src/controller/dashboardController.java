afda51d6fd318d5a2a37366f81333fd0a7085751
luvnico <ys2k6@mail.missouri.edu>
2018-05-30 22:29:50 -0600

    Added and displayed created/modified date for each post in dashboard, avoided sql credential exposure

--- Project/src/controller/dashboardController.java
+++ Project/src/controller/dashboardController.java
@@ -6,6 +6,11 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,13 +19,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
-import database.Account;
+import beans.Post;
+import database.Posting;
 
 /**
  * Servlet implementation class dashboardController
  */
-@WebServlet("/dashboardController")
-public class dashboardController extends HttpServlet {
+@WebServlet("/DashboardController")
+public class DashboardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private DataSource ds;
@@ -28,27 +34,37 @@ public class dashboardController extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public dashboardController() {
+    public DashboardController() {
         super();
         // TODO Auto-generated constructor stub
     }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see Servlet#init(ServletConfig)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(true);
-		PrintWriter out = response.getWriter();
-		request.getRequestDispatcher("/dashboard.jsp").forward(request, response);
-	}
+	public void init(ServletConfig config) throws ServletException {
+		try {
+			InitialContext initContext = new InitialContext();
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			Context env = (Context) initContext.lookup("java:comp/env");
+
+			ds = (DataSource) env.lookup("jdbc/CraigsList");
+
+		} catch (NamingException e) {
+			throw new ServletException();
+		}
+	}
+	
+    /**
+     * retrieve posting info from database
+     */
+	protected void retrieveData(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(true);
+		//get email from session
+		String email = session.getAttribute("loginEmail") == null ? 
+				(String)session.getAttribute("signupEmail") : (String)session.getAttribute("loginEmail");
+				
 		PrintWriter out = response.getWriter();
-		
 		Connection conn = null;
 
 		try {
@@ -56,19 +72,45 @@ public class dashboardController extends HttpServlet {
 			out.println("Connected to database.");
 		} catch (SQLException e) {
 			out.println("Unable to connect to database.");
-			System.out.println("Unable to connect to database.");
 			return;
-			//throw new ServletException();
 		}
+		
+		//if the user has not logged in yet
+		if(email == null){
+			//redirect to sign-up page
+			response.sendRedirect("signup.jsp");
+		}else{
+			//if the user has successfully logged in
+			try {
+				Posting p_conn = new Posting(conn);
+				//retrieve posting info from account
+				ArrayList<Post> result = p_conn.selectPosting(email);
+				request.setAttribute("postings", result);
+				int count = p_conn.countPosting(email);
+				request.setAttribute("count", count);
+				//forward posting info to dashboard.jsp
+				request.getRequestDispatcher("/dashboard.jsp").forward(request, response);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//System.out.println("get is called!");
+		retrieveData(request, response);
+	}
 
-		Account account = new Account(conn);
-		try {
-			ArrayList<String> result = account.selectPosting();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		request.setAttribute("postings", "result");
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//System.out.println("post is called!");
+		retrieveData(request, response);
 	}
 
 }
